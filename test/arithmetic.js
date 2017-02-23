7a046f3c10213dc0a8ef1b03d93583dca2f223a9
Alan Lu <alanlu1023@gmail.com>
2017-02-23 03:22:51 -0600

    Viewtiful Joe

--- test/arithmetic.js
+++ test/arithmetic.js
@@ -1,15 +1,17 @@
 var BigNumber = require('bignumber.js');
-BigNumber.config({ ROUNDING_MODE: BigNumber.ROUND_FLOOR });
+BigNumber.config({
+    ROUNDING_MODE: BigNumber.ROUND_FLOOR
+});
 
 let crypto;
 try {
-  crypto = require('crypto');
+    crypto = require('crypto');
 } catch (err) {
-  console.log('crypto support is disabled!');
+    console.log('crypto support is disabled!');
 }
 
 function rand256BitBigNumber() {
-    if(crypto) {
+    if (crypto) {
         return new BigNumber(crypto.randomBytes(32).toString('hex'), 16);
     }
 }
@@ -17,14 +19,16 @@ function rand256BitBigNumber() {
 var Arithmetic = artifacts.require("./Arithmetic.sol");
 
 contract('Arithmetic', function(accounts) {
-  for (var i = 1; i <= 10; i++) {
-  it(`should calculate a * b / d correctly, try ${i}`, function() {
-      var a = rand256BitBigNumber(), b = rand256BitBigNumber(), d = rand256BitBigNumber();
-    return Arithmetic.deployed().then(function(instance) {
-      return instance.overflowResistantFraction.call(a, b, d);
-  }).then(function(c) {
-      assert(c.equals( a.times(b).divToInt(d) ), `0x${c.toString(16)} != 0x${a.toString(16)} * 0x${b.toString(16)} / 0x${d.toString(16)}"`);
-    });
-  });
-}
+    for (var i = 1; i <= 10; i++) {
+        it(`should calculate a * b / d correctly, try ${i}`, function() {
+            var a = rand256BitBigNumber(),
+                b = rand256BitBigNumber(),
+                d = rand256BitBigNumber();
+            return Arithmetic.deployed().then(function(instance) {
+                return instance.overflowResistantFraction.call(a, b, d);
+            }).then(function(c) {
+                assert(c.equals(a.times(b).divToInt(d)), `0x${c.toString(16)} != 0x${a.toString(16)} * 0x${b.toString(16)} / 0x${d.toString(16)}"`);
+            });
+        });
+    }
 });
