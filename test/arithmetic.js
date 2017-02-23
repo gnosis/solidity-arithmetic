62dd4d3da8a07021c3d9e60c23a7051ba7a8dca2
Alan Lu <alanlu1023@gmail.com>
2017-02-23 03:21:48 -0600

    Clearly I should not be allowed near a computer

--- test/arithmetic.js
+++ test/arithmetic.js
@@ -1,11 +1,30 @@
+var BigNumber = require('bignumber.js');
+BigNumber.config({ ROUNDING_MODE: BigNumber.ROUND_FLOOR });
+
+let crypto;
+try {
+  crypto = require('crypto');
+} catch (err) {
+  console.log('crypto support is disabled!');
+}
+
+function rand256BitBigNumber() {
+    if(crypto) {
+        return new BigNumber(crypto.randomBytes(32).toString('hex'), 16);
+    }
+}
+
 var Arithmetic = artifacts.require("./Arithmetic.sol");
 
 contract('Arithmetic', function(accounts) {
-  it("should calculate a * b / d correctly", function() {
+  for (var i = 1; i <= 10; i++) {
+  it(`should calculate a * b / d correctly, try ${i}`, function() {
+      var a = rand256BitBigNumber(), b = rand256BitBigNumber(), d = rand256BitBigNumber();
     return Arithmetic.deployed().then(function(instance) {
-      return instance.overflowResistantFraction.call(2, 2, 3);
+      return instance.overflowResistantFraction.call(a, b, d);
   }).then(function(c) {
-      assert.equal(c.valueOf(), 1, "c != a * b / d");
+      assert(c.equals( a.times(b).divToInt(d) ), `0x${c.toString(16)} != 0x${a.toString(16)} * 0x${b.toString(16)} / 0x${d.toString(16)}"`);
     });
   });
+}
 });
