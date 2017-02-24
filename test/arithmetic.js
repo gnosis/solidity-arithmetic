1d53e724b112cdfcfc6f9f88d7c5d169cd38dbe3
Alan Lu <alanlu1023@gmail.com>
2017-02-23 22:18:17 -0600

    Added more tests

--- test/arithmetic.js
+++ test/arithmetic.js
@@ -5,11 +5,17 @@ BigNumber.config({
 
 let crypto = require('crypto');
 
+function rand128BitBigNumber() {
+    return new BigNumber(crypto.randomBytes(16).toString('hex'), 16);
+}
+
 function rand256BitBigNumber() {
     return new BigNumber(crypto.randomBytes(32).toString('hex'), 16);
 }
 
 var Arithmetic = artifacts.require("./Arithmetic.sol");
+const pow2To256 = new BigNumber(2).pow(256),
+    pow2To128 = new BigNumber(2).pow(128);
 
 contract('Arithmetic', function(accounts) {
     for (var i = 1; i <= 10; i++) {
@@ -20,10 +26,27 @@ contract('Arithmetic', function(accounts) {
                 return instance.mul256By256.call(a, b);
             }).then(function(cvals) {
                 var res = a.times(b),
-                    pow2To256 = new BigNumber(2).pow(256),
-                    pow2To128 = new BigNumber(2).pow(128),
                     c = cvals[0].times(pow2To256).plus(cvals[1].times(pow2To128)).plus(cvals[2]);
                 assert(c.equals(res), `Contract value doesn't match BigNumber value:\n0x${c.toString(16)} !=\n0x${res.toString(16)}\nformat(\n 0x${a.toString(16)} *\n 0x${b.toString(16)}\n, '02x')`);
+            });
+        });
+    }
+
+    for (var i = 1; i <= 10; i++) {
+        it(`should calculate a / b, try ${i}`, function() {
+            var a21 = rand256BitBigNumber(),
+                a0 = rand128BitBigNumber(),
+                b1 = rand128BitBigNumber(),
+                b0 = rand128BitBigNumber(),
+                a = a21.times(pow2To128).plus(a0),
+                b = b1.times(pow2To128).plus(b0);
+            return Arithmetic.deployed().then(function(instance) {
+                return instance.div256_128By128_128.call(a21, a0, b, b1, b0);
+            }).then(function(qr) {
+                var resQ = a.divToInt(b),
+                    resR = a.mod(b);
+                assert(qr[0].equals(resQ), `Contract value doesn't match BigNumber value:\n0x${qr[0].toString(16)} !=\n0x${resQ.toString(16)}\nformat(\n 0x${a.toString(16)} //\n 0x${b.toString(16)}\n, '02x')`);
+                assert(qr[1].equals(resR), `Contract value doesn't match BigNumber value:\n0x${qr[1].toString(16)} !=\n0x${resQ.toString(16)}\nformat(\n 0x${a.toString(16)} %\n 0x${b.toString(16)}\n, '02x')`);
             });
         });
     }
