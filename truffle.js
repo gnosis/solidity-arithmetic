067063df77ee2fe808a6164b3f83e5974edbeffb
Alan Lu <alanlu1023@gmail.com>
2017-02-24 18:54:27 -0600

    Packaging

--- truffle.js
+++ truffle.js
@@ -1,9 +1,18 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+
+// 12-word mnemonic
+var mnemonic = "congress drastic play goose cactus shine begin fork label panel churn potato";
+
 module.exports = {
   networks: {
     development: {
       host: "localhost",
       port: 8545,
       network_id: "*" // Match any network id
+    },
+    ropsten: {
+      provider: new HDWalletProvider(mnemonic, "https://ropsten.infura.io/"),
+      network_id: 3 // official id of the ropsten network
     }
   }
 };
